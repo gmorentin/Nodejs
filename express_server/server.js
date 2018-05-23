d16b6b605ea48e7142b0e73feb6fce8b1c51c8af
Gustavo Morentin <gmorentin@ucol.mx>
2018-05-23 08:34:42 -0500

    express y nodemon

---
+++ express_server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+let app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+    res.send({
+        name: 'Andrew',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    });
+});
+
+app.get('/about', (req,res) =>{
+    res.send('About Page');
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000);
